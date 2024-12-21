542055ed1f25854c537b2e1e1924d506794424f5
nastya-varenik <166423152+nastya-varenik@users.noreply.github.com>
2024-12-22 02:08:20 +0300

    Add files via upload

---
+++ src/data/personalInfo.jsx
@@ -0,0 +1,11 @@
+const personalInfo = {
+  name: 'Веренич Анастасия',
+  description: 'Frontend developer',
+  education: 'БГУ механико-математический факультет (2 курс)',
+  englishLevel: 'B2',
+  email: 'nastya_verenich@mail.ru',
+  telegram: 'https://t.me/nastya_varenik',
+  photo: 'https://i.imgur.com/hseVK81.jpg', // Ссылка на фотографию с Imgur
+};
+
+export default personalInfo;
---
+++ src/data/projects.jsx
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    title: 'Minisweepers',
+    description: 'Браузерная версия классической игры "Сапёр". Игрок открывает клетки на поле, избегая взрывов. Клетки могут быть либо пустыми, либо содержать мины.',
+    githubLink: 'https://nastya-varenik.github.io/Minesweepers/',
+    image: 'https://i.imgur.com/SntiFrW.gif', // Ссылка на изображение с Imgur
+  },
+  {
+    title: 'Lamoda',
+    description: 'Интернет-магазин с возможностью фильтрации и сортировки продуктов. Технологии: React, React-DOM, React-Icons',
+    githubLink: 'https://nastya-varenik.github.io/Lamoda/',
+    image: 'https://i.imgur.com/ZyD0seS.gif', // Ссылка на изображение с Imgur
+  },
+  {
+    title: 'Todo List',
+    description: 'Простой список задач (Todo List) с возможностью добавления, удаления и отметки задач как выполненных.',
+    githubLink: 'https://nastya-varenik.github.io/todo-list/',
+    image: 'https://i.imgur.com/nYdKRdL.gif', // Ссылка на изображение с Imgur
+  },
+  {
+    title: 'API & SPA',
+    description: 'Многостраничное приложение, использующее внешний API и библиотеку react-router-dom. Приложение включает страницы пользователей, альбомов и фотографий, а также страницу 404.',
+    githubLink: 'https://nastya-varenik.github.io/api-spa',
+    image: 'https://i.imgur.com/OJC7OZ3.gif', // Ссылка на изображение с Imgur
+  },
+];
+
+export default projects;
